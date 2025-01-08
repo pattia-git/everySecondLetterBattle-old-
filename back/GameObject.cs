ff663561f4141af5706e4b4296845d09c31040af
Patrik söderberg <patrik@dbi.nu>
2025-01-08 12:01:42 +0100

    game object

--- back/GameObject.cs
+++ back/GameObject.cs
@@ -1,4 +1,6 @@
-﻿namespace EverySecondLetterBattle;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace EverySecondLetterBattle;
 
 /*
 Contains game related data
@@ -11,7 +13,40 @@ Contains game related data
 
 public class GameObject
 {
+    public bool whosTurn;
+    public int player1Points;
+    public int player2Points;
+    public List<string> usedWords;
+    public List<string> player1Tiles;
+    public List<string> player2Tiles;
+
     public GameObject(){}
+
+
+    public void checkUsedWords()
+    {
+        
+    }
+    public void checkValidWord()
+    {
+        
+    }
     
+    
+
+    public void Player1Points()
+    {
+        foreach(var tile in guessedWord)
+        {
+            player1Points += 1;
+        }
+    }
+    public void Player2Points()
+    {
+        foreach (var tile in guessedWord)
+        {
+            player2Points += 1;
+        }
+    }
     
 }
